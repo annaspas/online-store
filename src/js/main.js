a9abb14753fc659d1621a84b1fea1e9e8de8c0ff
PhoenixDev - PG <pavel.grudina@phoenix-media.eu>
2019-07-07 20:50:17 +0300

    blablabla

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,18 @@
-<script>
-function myFunction(){
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } 
-  else {
-    x.className = "topnav";
-  }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
-</script>
 
+function toggleDropdown() {
+    let btn = document.getElementsByClassName('header_dropdown-main')[0];
 
+    if (btn.style.visibility === "" || btn.style.visibility === "hidden") {
+        btn.style.visibility = "visible";
+    } else {
+        btn.style.visibility = "hidden";
+    }
+}
