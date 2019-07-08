ce7bc5c0a8f3663e980a2cb8d2e8aef4b5848b31
annaspas <annaspasenko@gmail.com>
2019-07-08 17:59:25 +0300

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,23 @@ function toggleDropdown() {
         btn.style.visibility = "hidden";
     }
 }
+
+$(document).ready(function() {
+    $('.js_tabs-link').on('click', function() {
+        var tabNum = $(this).attr('href')
+        $(this).addClass('tab-active')
+        $(this)
+            .parent()
+            .siblings()
+            .find('.js_tabs-link')
+            .removeClass('tab-active')
+        $(tabNum).show(400)
+        $(tabNum).addClass('.tab-active')
+        $(tabNum)
+            .siblings()
+            .hide(400)
+        $(tabNum)
+            .siblings()
+            .removeClass('.tab-active')
+    })
+})
