715143c0082bf1682919420f18fbdf577a9614e3
annaspas <annaspasenko@gmail.com>
2019-07-10 15:27:13 +0300

    fixed

--- src/js/main.js
+++ src/js/main.js
@@ -18,16 +18,6 @@ $(document).ready(function() {
     })
 });
 
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 function toggleDropdown() {
     let btn = document.getElementsByClassName('header_dropdown-main')[0];
 
