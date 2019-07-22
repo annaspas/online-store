9084a25d87deebee73c875248fedec242cced983
annaspas <annaspasenko@gmail.com>
2019-07-22 18:30:07 +0300

    aligned

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('.js_login-modal').magnificPopup({
         type: 'inline',
@@ -7,18 +7,18 @@ $(document).ready(function() {
         }
     });
 
-    $('.form_input-file').on('change', function() {
+    $('.form_input-file').on('change', function () {
         var fileVal = $(this).val();
         $(this).next('.form_file-fake').find('.form_file').text(fileVal);
     })
 
-$('.js-login').on('click', function(){
-    $('.header_btns').slideToggle(400)
+    $('.js-login').on('click', function () {
+        $('.header_btns').slideToggle(400)
     })
-}); 
+});
 
-$(document).ready(function() {
-    $('.js_tabs-link').on('click', function() {
+$(document).ready(function () {
+    $('.js_tabs-link').on('click', function () {
         var tabNum = $(this).attr('href')
         $(this).addClass('tab-active')
         $(this)
@@ -37,71 +37,71 @@ $(document).ready(function() {
     })
 
 
-$('.trending_slides').slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 4,
-    centerMode: true,
-    buttons: false,
-    prevArrow: false,
-    nextArrow: false,
-    responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToshow:3
+    $('.trending_slides').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 4,
+        centerMode: true,
+        buttons: false,
+        prevArrow: false,
+        nextArrow: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToshow: 3
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToshow: 2,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToshow: 1,
+                }
             }
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToshow:2,
-                centerMode: true,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToshow:1,
-            }
-        }
-    ]
-});
+        ]
+    });
 
 
-if ($('.js_range').length > 0) {
-    $(".js_range").ionRangeSlider({
-        type: "double",
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$",
-        skin: "round"
-         });
-        }
+    if ($('.js_range').length > 0) {
+        $(".js_range").ionRangeSlider({
+            type: "double",
+            min: 0,
+            max: 1000,
+            from: 0,
+            to: 600,
+            prefix: "$",
+            skin: "round"
+        });
+    }
 
-    if ($('.js_card').length > 0 && $('.js_list').length > 0) { 
-        $('.js_card').on('click',function(){
+    if ($('.js_card').length > 0 && $('.js_list').length > 0) {
+        $('.js_card').on('click', function () {
             $(this).addClass('active');
             $(this).siblings().removeClass('active');
             $('.content_row').removeClass('content_list').addClass('content_card');
         });
 
-        $('.js_list').on('click',function(){
+        $('.js_list').on('click', function () {
             $(this).addClass('active');
             $(this).siblings().removeClass('active');
             $('.content_row').removeClass('content_card').addClass('content_list');
         });
     }
-    }); 
+});
 
-    if ($(window).width() < 768) {
-        $('.sidebar_title').on('click', function() {
-            $(this).next().slideToggle(400);
-            $(this).parent().siblings().find('.sidebar_main').slideUp(400);
-        });
-    }
+if ($(window).width() < 768) {
+    $('.sidebar_title').on('click', function () {
+        $(this).next().slideToggle(400);
+        $(this).parent().siblings().find('.sidebar_main').slideUp(400);
+    });
+}
 
 
 
